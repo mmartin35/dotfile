dd635a2cf54e2f4ceaaa7d426ba52bdd2df0a58d
user <matthieu1.martin@epitech.eu>
2022-10-07 11:20:50 +0200

    yes

--- config/epitech/builder.sh
+++ config/epitech/builder.sh
@@ -3,6 +3,6 @@ if [ -z $1 ];then
 	echo "no name specified"
 	exit 0
 else
-	cp ~/GIT/config/config/epitech/header ./$1
+	cp ~/GIT/dotfile/config/epitech/header ./$1.c
 	echo "build: done"
 fi
--- config/epitech/pusher.sh
+++ config/epitech/pusher.sh
@@ -1,20 +1,8 @@
-# return 0
-if [ -z $1 ];then 
-	echo "no commit message"
-	exit 0
-fi
-if [ $1 == "EasterEgg" ];then 
-	echo "In progress... Go on pastebin.com/testeurs"
-fi
-# return 
-echo "Do you want to push ? "
-read ans
-if [ $ans == "y" ];then
-	git add .
-	git pull
-	git commit -m "$1"
-	git push
-else 
-	echo "error"
-	exit 0 
-fi
+echo "Write down your commit :"
+read commit
+git add .
+git pull
+git commit -m $commit
+git push
+clear
+echo "git pushed successfully"
