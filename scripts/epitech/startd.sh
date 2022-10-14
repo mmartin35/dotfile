cbb7209a76df379885b59e99f675d00b272eaf3e
user <matthieu1.martin@epitech.eu>
2022-10-14 15:38:21 +0200

    ok

--- scripts/epitech/startd.sh
+++ scripts/epitech/startd.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 echo "Which day is it ?" 
 read day
-firefox https://github.com/EpitechPromo2027/B-CPE-100-NCY-1-1-cpoolday0$day-matthieu1.martin &
-firefox https://intra.epitech.eu/module/2022/B-CPE-100/NCY-1-1/#!/all/TP-$day-C-Pool-Day-0$day &
-firefox https://outlook.office365.com/mail/ &
+if [ $day < 10 ];then
+	firefox https://github.com/EpitechPromo2027/B-CPE-100-NCY-1-1-cpoolday0$day-matthieu1.martin &
+	firefox https://intra.epitech.eu/module/2022/B-CPE-100/NCY-1-1/#!/all/TP-$day-C-Pool-Day-0$day &
+	firefox https://outlook.office365.com/mail/ &
+else
+	firefox https://github.com/EpitechPromo2027/B-CPE-100-NCY-1-1-cpoolday$day-matthieu1.martin &
+        firefox https://intra.epitech.eu/module/2022/B-CPE-100/NCY-1-1/#!/all/TP-$day-C-Pool-Day-$day &
+        firefox https://outlook.office365.com/mail/ &
 echo "Do you want to clone the repo ? (y/n)"
 read ans
 if [ $ans = "y" ];then
-	git clone git@github.com:EpitechPromo2027/B-CPE-100-NCY-1-1-cpoolday0$day-matthieu1.martin.git
-	mv B-CPE* ~/GIT/epitech/day$day
+	if [ $day < 10 ];then
+		git clone git@github.com:EpitechPromo2027/B-CPE-100-NCY-1-1-cpoolday0$day-matthieu1.martin.git
+		mv B-CPE* ~/GIT/epitech/day$day
+	else
+		git clone git@github.com:EpitechPromo2027/B-CPE-100-NCY-1-1-cpoolday$day-matthieu1.martin.git
+                mv B-CPE* ~/GIT/epitech/day$day
 else
 	echo "Repo not cloned"
 fi
