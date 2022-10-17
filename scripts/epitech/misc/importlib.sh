0da0b989e457df9c7414bd792066af81ba067a96
user <matthieu1.martin@epitech.eu>
2022-10-17 18:14:10 +0200

    libimport_update

--- scripts/epitech/misc/importlib.sh
+++ scripts/epitech/misc/importlib.sh
@@ -1,7 +1,7 @@
 echo "Do you want to add lib to this folder ? y/n"
 read ans
 if [ $ans = 'y' ];then
-	cp ~/GIT/epitech/ressources/ .
+	cp -r ~/GIT/epitech/ressources/ .
 	echo "lib imported"
 else
 	echo "aborted"
