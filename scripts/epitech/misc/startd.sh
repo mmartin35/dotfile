b1aadf0521ce25f3b092ef09194f8d06aa6916b9
user <matthieu1.martin@epitech.eu>
2022-10-20 15:22:01 +0200

    ok

--- scripts/epitech/misc/startd.sh
+++ scripts/epitech/misc/startd.sh
@@ -4,11 +4,9 @@ read day
 if [ $day < 10 ];then
 	firefox https://github.com/EpitechPromo2027/B-CPE-100-NCY-1-1-cpoolday0$day-matthieu1.martin &
 	firefox https://intra.epitech.eu/module/2022/B-CPE-100/NCY-1-1/#!/all/TP-$day-C-Pool-Day-0$day &
-	firefox https://outlook.office365.com/mail/ &
 else
 	firefox https://github.com/EpitechPromo2027/B-CPE-100-NCY-1-1-cpoolday$day-matthieu1.martin &
         firefox https://intra.epitech.eu/module/2022/B-CPE-100/NCY-1-1/#!/all/TP-$day-C-Pool-Day-$day &
-        firefox https://outlook.office365.com/mail/ &
 fi
 clear
 echo "Do you want to clone the repo ? (y/n)"
