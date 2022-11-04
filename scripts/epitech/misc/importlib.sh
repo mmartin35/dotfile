b2253aa9e77b66e4c5cb2c5794eaccbff39a2eb3
user35p74a <matthieu1.martin@epitech.eu>
2022-11-04 19:05:26 +0100

    update

--- scripts/epitech/misc/importlib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Do you want to add lib to this folder ? y/n"
-read ans
-if [ $ans = 'y' ];then
-	cp -r ~/GIT/epitech/lib_epitech/* .
-	echo "lib imported"
-else
-	echo "aborted"
-fi
---
+++ scripts/utils/pushers/g_add.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+add = 
+git add .
+git commit -m "adding: $add"
+git push
+echo "=============================="
+echo "| git folder has been pushed |"
+echo "=============================="
