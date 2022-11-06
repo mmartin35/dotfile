f4e755ebbcf7b4e714d4f4f48e5467f3524da3dc
user35p74a <matthieu1.martin@epitech.eu>
2022-11-06 16:50:19 +0100

    ---

--- scripts/utils/github/classic.sh
+++ scripts/utils/github/classic.sh
@@ -1,7 +1,7 @@
 echo "Write down your commit :"
 read commit
 git add .
-git commit -m $commit
+git commit -m "$commit"
 git push
 echo "=============================="
 echo "| git folder has been pushed |"
