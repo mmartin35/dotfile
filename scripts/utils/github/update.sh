9b2410c0880e4d40d2463b55a51c2b4c6202a566
user35p74a <matthieu1.martin@epitech.eu>
2022-11-09 14:39:55 +0100

    ---

--- scripts/utils/github/update.sh
+++ scripts/utils/github/update.sh
@@ -1,7 +1,8 @@
-echo "Write down your commit :"
-read commit
+#!/bin/bash
+args=echo (git status | grep 'modified:' | sed 's/^.*: //' | tr -d '\n')
+
 git add .
-git commit -m $commit
+git commit -m "updated: $args"
 git push
 echo "=============================="
 echo "| git folder has been pushed |"
