8bbb7e20a99ba8d1893c1d006de9cd312e6a5f96
user35p74a <matthieu1.martin@epitech.eu>
2022-11-12 18:40:29 +0100

    | 🚀 Updated: | 🔋 Advancement: 12% | ✏ Added: | ✂ Deleted:

--- scripts/utils/github/classic.sh
+++ scripts/utils/github/classic.sh
@@ -4,7 +4,8 @@ argsmodify=$(git status | grep 'modified:' | sed 's/^.*: //' | tr -d '\n')
 argsdel=$(git status | grep 'deleted:' | sed 's/^.*: //' | tr -d '\n')
 argsadd=$(git status | grep -A1 '"git add <file>..." to include' | tail -1 | tr -d '\n')
 
-echo "Advancement (..%):" && read advancement
+echo "Advancement (..%):"
+read advancement
 
 #	TEST		#
 if [ -n $argsmofidy ];then
