ef5f2910ae630fd6cd0b327215c7842427c56714
user35p74a <matthieu1.martin@epitech.eu>
2022-11-13 16:57:24 +0100

    | 🚀 Updated:  create.sh  | 🔋 Advancement: %  | > Comment:   | ✏ Added:  | ✂ Deleted:  |

--- scripts/utils/github/create.sh
+++ scripts/utils/github/create.sh
@@ -6,5 +6,5 @@ gh auth login
 gh repo create $NAME --private --source=. --remote=upstream --push
 git push
 echo "=============================="
-echo "| git folder has been created |"
+echo "|   git folder is created    |"
 echo "=============================="
