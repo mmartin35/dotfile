ddbf0679c11b65de19c7451e5a031a88c8a27ff0
user35p74a <matthieu1.martin@epitech.eu>
2022-11-22 15:36:59 +0100

    > updating:  |  > comment:  |  > added: scripts/utils/render/ |

---
+++ scripts/utils/render/randrr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ -z $1 ] || [ $1 == "-h" ];then
+	echo "USAGE                                                    "
+	echo "      command format: randrr [option] [output] [position]"
+	echo "OPTIONS                                                  "
+	echo "      1. reset to main position (single display)         "
+	echo "      2. set output and position                         "
+	echo "      3. not setup yet                                   "
+	exit 0
+fi
+if [ $1 == "1" ];then
+	xrandr --auto --output eDP-1 --mode 1920x1080
+	echo "status: position reset"
+elif [ $1 == "2" ];then
+	if [ -z $2 ] || [ -z $3 ];then
+		echo "status: wrong number of argument"
+		exit 1
+	else
+        	xrandr --auto --output eDP-1 --mode 1920x1080 --$3-of $2
+		echo "status: position set"
+	fi
+elif [ $1 == "3" ];then
+	echo "status: not setup yet"
+else
+	echo "error"
+	exit 1
+fi
+exit 0;
