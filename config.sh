750da07a46fc613dbbbb17f38e8f24bdffaf836e
laptop <matthieu1.martin@epitech.eu>
2023-05-06 20:06:01 +0200

    config.sh script added

--- config.sh
+++ config.sh
@@ -1,35 +1,65 @@
 #! /bin/bash
 /bin/clear
 
+_install_nvim_fs() {
+    return
+}
+
 #           INSTALL         #
 
+_build_dirs() {
+    return
+}
+
 _setup_auths() {
+    # ssh key #
+    ssh-keygen -f %HOME/.ssh/ssh_auth -N ""
     return
 }
 
 _install_soft() {
-    return
-}
-
-_build_sym_links() {
+    sudo apt install zsh git docker.io kitty
+    _install_nvim_fs
     return
 }
 
 _get_epitech_resources() {
+    sudo docker pull epitechcontent/epitest-docker
     return
 }
 
 _setup_preferencies() {
+    cp -r $PWD/config/nvim $HOME/.config
+
+    cp $PWD/dot/zshrc $HOME/.zshrc
+    cp -r $PWD/dot/zshrc_conf $HOME/.zshrc_conf
+    return
+}
+
+_build_sym_links() {
+    # scripts #
+    sudo ln -s $PWD/scripts/git/auto_commit.sh /usr/local/bin/auto_commit
+    sudo ln -s $PWD/scripts/headers/epitech_header.sh /usr/local/bin/header
+    sudo ln -s $PWD/scripts/tests/coding_style.sh /usr/local/bin/coding-style
+
+    exa /usr/local/bin
     return
 }
 
 _update_all() {
+    sudo apt update
+    sudo apt upgrade
     return
 }
 
 #           SAVE            #
 
 _save_config() {
+    cd $HOME/GIT/dotfile
+    cp $HOME/.config/nvim/init.vim $PWD/config/nvim/
+
+    cp $HOME/.zshrc $PWD/dot/zshrc
+    cp -r $HOME/.zshrc_conf/* $PWD/dot/zshrc_conf/
     return
 }
 
@@ -44,16 +74,19 @@ if [ -z $status ]; then
     exit 84
 fi
 if [ $status == "y" ]; then
+    _build_dirs
     _setup_auths
     _install_soft
-    _build_sym_links
     _get_epitech_resources
     _setup_preferencies
+    _build_sym_links
     _update_all
+    echo "Install done."
     exit 0
 elif [ $status == "save" ]; then
     _save_config
     _push_changes
+    echo "Save done."
     exit 0
 else
     echo "aborted"
--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -1,11 +1,48 @@
-set tabstop=4
-set tabstop=4
 set expandtab
+set tabstop=4
 set shiftwidth=4
-set cc=80
 set mouse=a
+
+function! InsertStatuslineColor(mode)
+    if a:mode == 'i'
+        hi statusline ctermfg=3 ctermbg=0
+    elseif a:mode == 'r'
+        hi statusline ctermfg=4 ctermbg=0
+    else
+        hi statusline ctermfg=1 ctermbg=0
+    endif
+endfunction
+
+function! GitBranch()
+    return system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
+endfunction
+function! StatuslineGit()
+    let l:branchname = GitBranch()
+    return strlen(l:branchname) > 0?'  '.l:branchname.' ':''
+endfunction
+
+function! User()
+    return system("echo $USER | tr -d '\n'")
+endfunction
+
+au InsertEnter * call InsertStatuslineColor(v:insertmode)
+au InsertLeave * hi statusline ctermfg=7 ctermbg=0
+hi statusline ctermfg=7 ctermbg=0
+
+set statusline=
+set statusline+=[%{StatuslineGit()}]
+set statusline+=\ at
+set statusline+=\ %F
+set statusline+=%=%l
+
 cnoreabbrev tree NERDTree
 
 call plug#begin('~/.config/nvim/plugged')
  Plug 'scrooloose/nerdtree'
+ Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+ Plug 'windwp/nvim-autopairs'
 call plug#end()
+
+lua << EOF
+require("nvim-autopairs").setup {}
+EOF
